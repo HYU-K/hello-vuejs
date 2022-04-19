c4159af6e1e7826633f4083b322efc0b7f685ef5
HYU-K <85468352+HYU-K@users.noreply.github.com>
2022-04-19 22:22:42 +0700

    add

--- src/components/CompTabs.vue
+++ src/components/CompTabs.vue
@@ -1,28 +1,58 @@
 <template>
-    <div class="comp-tabs">
-        <div class="tabs-title">
-            <span>{{ title }}</span>
-        </div>
-        <div class="tabs-content">
-            <slot></slot>
-        </div>
+  <div class="comp-tabs">
+    <div class="tabs-title">
+      <span>{{ title }}</span>
     </div>
+    <div class="tabs-content">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'comp-tabs',
-    props: {
-        title: String
-    },
-    data() {
-        return {}
-    },
-    methods: {
-    }
-}
+  name: "comp-tabs",
+  props: {
+    title: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <style>
+.tabs-header {
+  display: flex;
+  align-items: center;
+  padding: 0;
+}
 
+.tabs-header li {
+  display: inline-block;
+  padding: 5px 10px;
+  background-color: #ccc;
+  cursor: pointer;
+  color: black;
+}
+
+.tabs-header li:hover {
+  background-color: black;
+  color: white;
+}
+
+.tabs-header .active {
+  background-color: black;
+  color: white;
+}
+
+.item-content {
+  border: 1px solid black;
+  padding: 20px;
+}
+
+.item-content .content {
+  text-align: left;
+}
 </style>
--- src/components/DemoSlot.vue
+++ src/components/DemoSlot.vue
@@ -2,11 +2,55 @@
   <div class="Demo-slot">
     <h1>demo slot</h1>
     <comp-tabs title="Tiêu đề của tabs">
-        <ul class="tabs-header">
-            <li v-for="(item, index) in tapsHeader" v-bind:key="index">
-                {{ item.text }}
-            </li>
-        </ul>
+      <ul class="tabs-header">
+        <li
+          v-for="(item, index) in tapsHeader"
+          v-bind:key="index"
+          v-on:click="setActivetabs(index)"
+          v-bind:class="{ active: tabActive == index }"
+        >
+          {{ item.text }}
+        </li>
+      </ul>
+      <div class="item-content">
+        <div class="content tab1" v-if="tabActive === 0">
+          <h3>tab 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+            natus quia, nobis, obcaecati perspiciatis laboriosam aut veniam
+            repellendus voluptatibus fugit voluptatum impedit! Cumque fugit
+            natus necessitatibus voluptates nesciunt rerum veritatis!
+          </p>
+          <p>
+            quia, nobis, obcaecati perspiciatis laboriosam aut veniam
+            repellendus voluptatibus fugit voluptatum imp
+          </p>
+        </div>
+        <div class="content tab1" v-else-if="tabActive === 1">
+          <h3>tab 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+            natus quia, nobis, obcaecati perspiciatis laboriosam aut veniam
+            repellendus voluptatibus fugit voluptatum impedit! Cumque fugit
+            natus necessitatibus voluptates nesciunt rerum veritatis!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+            natus quia, nobis, obcaecati perspiciatis laboriosam aut veniam
+            repellendus voluptatibus fugit voluptatum impedit! Cumque fugit
+            natus necessitatibus voluptates nesciunt rerum veritatis!
+          </p>
+        </div>
+        <div class="content tab1" v-else>
+          <h3>tab 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+            natus quia, nobis, obcaecati perspiciatis laboriosam aut veniam
+            repellendus voluptatibus fugit voluptatum impedit! Cumque fugit
+            natus necessitatibus voluptates nesciunt rerum veritatis!
+          </p>
+        </div>
+      </div>
     </comp-tabs>
   </div>
 </template>
@@ -17,14 +61,16 @@ export default {
   name: "Demo-slot",
   data() {
     return {
-        tapsHeader: [
-            {text: 'tab 1'},
-            {text: 'tab 2'},
-            {text: 'tab 3'},
-        ]
-    }
+      tapsHeader: [{ text: "tab 1" }, { text: "tab 2" }, { text: "tab 3" }],
+      tabActive: 0,
+    };
   },
-  methods: {},
+  methods: {
+    setActivetabs(index) {
+      console.log(index);
+      this.tabActive = index;
+    },
+  },
   components: {
     CompTabs,
   },
@@ -33,9 +79,9 @@ export default {
 
 <style>
 .Demo-slot {
-    width: 800px;
-    margin: 20px auto;
-    padding: 50px;
-    border: 1px solid #000;
+  width: 800px;
+  margin: 20px auto;
+  padding: 50px;
+  border: 1px solid #000;
 }
 </style>
